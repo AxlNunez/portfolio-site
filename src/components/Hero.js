2108a5595e37be74b77f57a4a69bba52be6e1cd2
AxlNunez <axltico12@gmail>
2022-08-19 12:00:58 -0400

    final fixes to portfolio

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,28 +6,32 @@ export default function Hero() {
 
    return (
       <section className="hero">
-         <div className="hero-header">
-            <p className="hero-small-heading ">Hi, I'm</p>
-            <h1 className="hero-big-heading">Axl Nuñez,</h1>
-            <h1 className="hero-big-heading secondry-big-heading">I'm a </h1>
-            <p className="hero-desc">
-               My name is Axl Nuñez, I'm a Marine Corps veteran and a student at
-               Montclair State University, New Jersey. Currently, I am pursuing
-               a dual degree in Computer Science. Additionally, I am an
-               experienced Full-Stack Software Engineer, I build web
-               applications from the ground up.
-            </p>
-         </div>
-         <button
-            className="hero-btn primary-btn"
-            onClick={() => setDownload(true)}
-         >
-            <a href={Resume} download="AxlNunez-Resume">
-               <p>
-                  {download ? "Thanks For Your Interest !" : "Download Resume"}
+         <div>
+            <div className="hero-header">
+               <p className="hero-small-heading ">Hi, I'm</p>
+               <h1 className="hero-big-heading">Axl Nuñez,</h1>
+               <h1 className="hero-big-heading secondry-big-heading">I'm a </h1>
+               <p className="hero-desc">
+                  My name is Axl Nuñez, I'm a Marine Corps veteran and a student
+                  at Montclair State University, New Jersey. Currently, I am
+                  pursuing a dual degree in Computer Science. Additionally, I am
+                  an experienced Full-Stack Software Engineer, I build web
+                  applications from the ground up.
                </p>
-            </a>
-         </button>
+            </div>
+            <button
+               className="hero-btn primary-btn"
+               onClick={() => setDownload(true)}
+            >
+               <a href={Resume} download="AxlNunez-Resume">
+                  <p>
+                     {download
+                        ? "Thanks For Your Interest !"
+                        : "Download Resume"}
+                  </p>
+               </a>
+            </button>
+         </div>
       </section>
    );
 }
